54359702fdfc9f98abc059031a33e528e29900e2
= <=>
2024-04-17 23:06:47 -0400

    First Commit
    initial commit.

--- src/main/java/com/taykon/webserver/App.java
+++ src/main/java/com/taykon/webserver/App.java
@@ -1,13 +1,27 @@
 package com.taykon.webserver;
 
+import com.taykon.webserver.connections.readConnection;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+
+    static{
+        testLogger();
+    }
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Starting server:" );
+    }
+
+    public static void testLogger(){
+
+        readConnection con = new readConnection();
+        con.startServer();
+
     }
 }
---
+++ src/main/java/com/taykon/webserver/connections/serverState.java
@@ -0,0 +1,32 @@
+package com.taykon.webserver.connections;
+
+import java.util.Scanner;
+
+public class serverState implements Runnable{
+
+    private volatile String State;
+
+    public String getState() {
+        return State;
+    }
+
+    public void setState(String state) {
+        State = state;
+    }
+
+    @Override
+    public void run() {
+        
+
+        System.out.println("State:");
+        Scanner input = new Scanner(System.in);
+
+        State = input.nextLine();
+
+
+        throw new UnsupportedOperationException("Unimplemented method 'run'");
+    }
+    
+    
+
+}
